e0c9f8f94c01ed066e97143d1890d1948341d77f
Parnian <mp.anbary@gmail.com>
2021-04-25 17:30:55 +0430

    test commit

---
+++ particles.js
@@ -0,0 +1,27 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+document.getElementById("RangePicker").oninput = function () { RangeFunction() };
+
+function RangeFunction() {
+    const val = document.getElementById("RangePicker");
+    const output = document.getElementById("output");
+    output.innerHTML = val.value;
+
+}
+
+
+document.getElementById("controlsIcon").onclick = function () { clickControlsIcon() };
+
+function clickControlsIcon() {
+    const controls = document.getElementById("controlsIcon");
+    let x;
+    if (x == 0) {
+        controls.innerHTML = 'hellloooo';
+        x = 1;
+    }
+    else {
+        x = 0;
+        controls.innerHTML = 'byeeeee';
+    }
+}
